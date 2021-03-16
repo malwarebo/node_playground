c4183846de0dc06df87c2cb48bfe95721df1e085
Irfan <irfnhm@gmail.com>
2021-03-16 16:50:23 +0530

    adds modules and event emitters

--- questions.js
+++ questions.js
@@ -1,32 +1,21 @@
+const collectAnswers = require("./lib/collectAnswers");
+
 const questions = [
 	"what is your name?",
 	"what would you rather be doing?",
 	"what is your preferred programming language?"
 ];
 
-const ask = (i = 0) => {
-	process.stdout.write(`\n\n\n ${questions[i]}`);
-	process.stdout.write(` > `);
-};
+const answerEvents = collectAnswers(questions);
 
-ask();
+answerEvents.on("answer", answer => 
+    console.log(`question answered: ${answer}`)
+);
 
-const answers = [];
-process.stdin.on('data', data => {
-    answers.push(data.toString().trim());
+answerEvents.on("complete", answers => {
+    console.log("Thank you");
+    console.log(answers);
+    process.exit();
+} );
 
-    if(answers.length < questions.length) {
-        ask(answers.length);
-    } else {
-        process.exit();
-    }
-});
-
-process.on('exit', () => {
-    const[name, activity, lang] = answers;
-    console.log(`
-        Thank you for your answers.
-
-        Go ${activity} ${name} you can write ${lang} code later!
-    `);
-});
+answerEvents.on("complete", () => process.exit);
