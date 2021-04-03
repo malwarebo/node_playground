bbbcd55ffbf81fb7c046b2bd0d982d06a0de493f
Irfan <irfnhm@gmail.com>
2021-04-03 14:44:34 +0530

    adds eslint and prettier support

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,12 +1,18 @@
 const { response } = require('express');
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) => {
-    response.send('This is express app!');
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
