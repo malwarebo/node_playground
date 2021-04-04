efa1a1700fe215cc79d46da4a58e85026c40769b
Irfan <irfnhm@gmail.com>
2021-04-04 13:33:28 +0530

    add cookies-session and add dynamic page rendering capability

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const cookieSession = require('cookie-session');
 const FeedbackService = require('./services/FeedbackService');
 const SpeakerService = require('./services/SpeakerService');
 
@@ -8,14 +8,37 @@ const feedbackService = new FeedbackService('./data/feedback.json');
 const speakerService = new SpeakerService('./data/speakers.json');
 
 const routes = require('./routes');
+const { request } = require('http');
+const { response } = require('express');
 const app = express();
 
 const port = 3000;
-app.set('view engine', 'ejs');
 
+// Session management
+app.set('trust proxy', 1); // Allow the app to trust the cookie-session
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['Rand123sjdnwehr83', '47ry3fb38ry347dsubdwe7'],
+  })
+);
+
+// Set the view engine {ejs}
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'Roux Meetups';
 app.use(express.static(path.join(__dirname, './static')));
+
+app.use(async (request, response, next) => {
+  try {
+    const names = await speakerService.getNames();
+    response.locals.speakerNames = names;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 app.use(
   '/',
